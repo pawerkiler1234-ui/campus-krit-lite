aefb3752941435f03831223db2cdb633fa5266fd
Dazy <pawerkiler1234@gmail.com>
2026-01-06 16:59:24 +0530

    Fix Tailwind v4 apply issue by moving styles to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
